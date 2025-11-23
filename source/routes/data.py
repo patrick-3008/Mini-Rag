85faaeb1e1a8055f906dfe154c079059692fabb2
patrick-3008 <patricknaashat@yahoo.com>
2025-11-23 14:57:40 +0200

    add uploaded file to be proccessed

--- source/routes/data.py
+++ source/routes/data.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController
+from controllers import DataController, ProjectController
+from models import ResponseSignal
+import aiofiles
+import os
 
 data_router = APIRouter(
     prefix="/api/v1/data", # add prefix to all the urls
@@ -15,6 +19,27 @@ async def upload_data(
     ):
 
     # vaildate
-    is_vaild = DataController().validate_uploaded_file(file=file)
+    is_vaild, result_signal = DataController().validate_uploaded_file(file=file)
 
-    return is_vaild
+    if not is_vaild:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"signal": result_signal}
+        )
+
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path = os.path.join(
+        project_dir_path,
+        file.filename
+    )
+
+    async with aiofiles.open(file_path, "wb") as f:
+        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+            await f.write(chunk)
+
+    return JSONResponse(
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            }
+        )
+
