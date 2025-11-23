a3aaa4dbde07cc29b443ebd9cc6fb6bf7c555781
patrick-3008 <patricknaashat@yahoo.com>
2025-11-23 13:14:37 +0200

    add rested routes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from routes import base
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
 app = FastAPI()
 
-
-@app.get("/welcome")
-def welcome():
-    return {
-        "message": "Welcome to FastAPI."
-    }
+app.include_router(base.base_router)
