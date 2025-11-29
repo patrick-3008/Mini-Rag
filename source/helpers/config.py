9d6b04e07c7aacc0fb56438310cd2bb37f63ad17
patrick-3008 <patricknaashat@yahoo.com>
2025-11-29 16:43:50 +0200

    add llm provider factory

--- source/helpers/config.py
+++ source/helpers/config.py
@@ -14,6 +14,21 @@ class Settings(BaseSettings):
     MONGO_URI: str
     MONGO_DB_NAME: str
 
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    
+    INPUT_DEFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DEFAULT_MAX_TOKENS: int = None
+    GENERATION_DEFAULT_TEMPERATURE: float = None
+
     class Config:
         env_file = ".env"
 
---
+++ source/stores/llm/providers/__init__.py
@@ -0,0 +1,2 @@
+from .cohere_provider import CoHereProvider
+from .openai_provider import OpenAIProvider
