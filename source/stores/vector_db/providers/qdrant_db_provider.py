f3261e28ae1668cd9754d9dbc409aafc65b1cb48
patrick-3008 <patricknaashat@yahoo.com>
2025-11-29 20:32:02 +0200

    add some functions of VectorDBInterface

---
+++ source/stores/vector_db/providers/qdrant_db_provider.py
@@ -0,0 +1,39 @@
+from qdrant_client import models, QdrantClient
+from ..vector_db_interface import VectorDBInterface
+from ..VectorDBEnums import DistanceMethodEnums
+import logging
+from typing import List
+
+class QdrantDBProvider(VectorDBInterface):
+
+    def __init__(self, db_path: str, distance_method: str):
+        self.client = None
+        self.db_path = db_path
+        self.distance_method = None
+
+        if distance_method == DistanceMethodEnums.COSINE.value:
+            self.distance_method = models.Distance.COSINE
+        elif distance_method == DistanceMethodEnums.DOT.value:
+            self.distance_method = models.Distance.DOT
+
+        self.logger = logging.getLogger(__name__)
+
+    def connect(self):
+        self.client = QdrantClient(path = self.db_path)
+
+    def disconnect(self):
+        self.client = None
+
+    def is_collection_existed(self, collection_name: str) -> bool:
+        return self.client.collection_exists(collection_name = collection_name)
+    
+    def list_all_collections(self) -> List:
+        return self.client.get_collections()
+    
+    def get_collection_info(self, collection_name: str) -> dict:
+        return self.client.get_collection(collection_name = collection_name)
+    
+    def delete_collection(self, collection_name: str):
+        if self.is_collection_existed(collection_name):
+            return self.client.delete_collection(collection_name = collection_name)
+        
